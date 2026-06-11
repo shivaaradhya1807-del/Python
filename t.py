136d2796d2141b6f9c022dff5203f54f5bce4488
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-06-11 19:43:36 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+screen=turtle.Screen()
+screen.bgcolor("blue")
+screen.setup(600,600)
+
+t=turtle.Turtle()
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+
+
+turtle.done()
