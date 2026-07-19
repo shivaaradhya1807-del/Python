485d24a500f9d05edd95f6727b831df416d38e06
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-07-19 16:47:03 +0530

    Add files via upload

---
+++ pol_cricket.py
@@ -0,0 +1,13 @@
+class cricket :
+    def info(self):
+        print("Cricket score info :")
+
+class football:
+    def info(self):
+        print("Football score info :")
+
+c=cricket()
+f=football()
+
+c.info()
+f.info()
